4e781501a8a464787a601806881e2819cba4d13c
vladimir <vladi2909@outlook.com>
2020-12-30 14:07:43 +0300

    feat: add dark theme

--- project/src/App.js
+++ project/src/App.js
@@ -1,12 +1,31 @@
 
 import Layout from './hoc/Layout/Layout';
 import Quiz from './containers/Quiz/Quiz';
+import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import { Paper, Switch } from '@material-ui/core';
+import { useState } from 'react';
 
 function App() {
+
+  const [darkMode, setDarkMode] = useState(true);
+  const theme = createMuiTheme({
+    palette: {
+      type: darkMode ? 'dark' : 'light'
+    }
+  });
+
   return (
-      <Layout>
-        <Quiz />
-      </Layout>
+    <ThemeProvider theme={theme}>
+      <Paper>
+        <Layout>
+          <Switch
+            checked={darkMode}
+            onChange={() => setDarkMode(!darkMode)}
+          />
+          <Quiz />
+        </Layout>
+      </Paper>
+    </ThemeProvider>
   );
 }
 
